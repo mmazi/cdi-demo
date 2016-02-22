9698c41399aa523c5fbb0b38f13506f87993ac8c
Matija Mazi <matija.mazi@gmail.com>
2016-02-22 13:18:57 +0100

    Page title provider: observe log in event

--- src/main/java/si/mazi/javademo/cdi/backend/TitleProvider.java
+++ src/main/java/si/mazi/javademo/cdi/backend/TitleProvider.java
@@ -4,6 +4,7 @@ import si.mazi.javademo.cdi.model.User;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
@@ -14,12 +15,21 @@ public class TitleProvider {
 
     @Inject @LoggedIn Instance<User> userInstance;
 
+    private String requestTitle;
+
     @Produces @Dependent @Named("pageTitle")
     public String getPageTitle() {
+        if (requestTitle != null) {
+            return requestTitle;
+        }
         final User loggedInUser = userInstance.get();
         if (loggedInUser != null) {
             return "CDI / " + loggedInUser.getName();
         }
         return "CafeBabe CDI demo";
+    }
+
+    public void afterLogin(@Observes @LoggedIn Authenticator.LoginEvent event) {
+        requestTitle = event.user == null ? "Ooops.." : String.format("Welcome, %s!", event.user.getName());
     }
 }
